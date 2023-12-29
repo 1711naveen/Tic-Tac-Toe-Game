1f0634ea0e1b0a800650d7fd4503c357159f87c2
NaveenYadav <yn.naveen00@gmail.com>
2023-12-29 19:33:49 +0530

    Changed script.js

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ const changeTurn = () => {
         turn = "O";
     else
         turn = "X"
+
+        return turn;
 }
 
 let boxes = document.getElementsByClassName('box');
 console.log(boxes)
 
-Array.from(boxes).forEach((element) => {
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click', () => {
         if (boxtext.innerText === '') {
             boxtext.innerText = turn;
-            turn = changeTurn;
+            turn = changeTurn();
         }
     })
 })
