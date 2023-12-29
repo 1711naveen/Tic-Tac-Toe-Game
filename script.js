1f3e57af418d66f06842bdd37b5a9742051c0dfe
NaveenYadav <yn.naveen00@gmail.com>
2023-12-29 19:27:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let turn = "X";
+
+const changeTurn = () => {
+    if (turn === "X")
+        turn = "O";
+    else
+        turn = "X"
+}
+
+let boxes = document.getElementsByClassName('box');
+console.log(boxes)
+
+Array.from(boxes).forEach((element) => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn;
+        }
+    })
+})
